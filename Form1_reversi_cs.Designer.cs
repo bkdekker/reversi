0fcf19f1fed7a6b5857611adfa090adb652e033d
bkdekker <84399183+bkdekker@users.noreply.github.com>
2021-12-21 15:00:07 +0100

    Add files via upload

---
+++ Form1_reversi_cs.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.Reset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            //this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.Location = new System.Drawing.Point(12, 71);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(500, 500);
+            this.Panel.TabIndex = 0;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(12, 42);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(123, 23);
+            this.Reset.TabIndex = 1;
+            this.Reset.Text = "Nieuw Spel";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 49);
+            this.label1.Name = "Aandebeurt";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rood begint";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            //this.ResumeLayout(false);
+            //this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel Panel;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1_reversi_cs.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    public partial class Form1 : Form
+    {
+        int hor;
+        int vert;
+        int [,] vak;
+        int breedte;
+        int hoogte;
+        int beurt;
+        public Form1()
+        {
+            beurt = 1;
+            this.hor = 6;
+            this.vert = 6;
+            vak = new int[hor, vert];
+            int handig = (vert / 2) - 1;
+            for (int i = 0; i < vert; i++)
+            {
+
+                for (int j = 0; j < vert; j++)
+                {
+                    if ((i == 2 && j == 2) || (i == 3 && j == 3))
+                    {
+                        vak[i, j] = 1;
+                    }
+                    else if ((i == 2 && j == 3) || (i == 3 && j == 2))
+                    {
+                        vak[i, j] = 2;
+                    }
+                    else
+                    {
+                        vak[i, j] = 0;
+                    }
+                }
+            }
+
+            InitializeComponent();
+            breedte = Panel.Size.Width / hor;
+            hoogte = Panel.Size.Height / vert;
+            Panel.Paint += this.tekenveld;
+            Panel.MouseClick += this.zetwaardes;
+            Panel.Paint += this.tekencirkel;
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zetwaardes ( object sender, MouseEventArgs mea)
+        {
+            // if (mogelijk())
+            for (int a = 0; a < hor; a++)
+            {
+                if ((a* Panel.Size.Width / hor) < mea.X && mea.X < ((a + 1)* Panel.Size.Width / hor))
+                {
+                    for (int b = 0; b < vert; b++)
+                    {
+
+                        if ((b* Panel.Size.Height / vert) < mea.Y && mea.Y < ((b + 1)* Panel.Size.Height / vert))
+                            if (mogelijk(a,b))
+                            {
+                                this.vak[a, b] = beurt;
+                                if (beurt == 1)
+                                {
+                                    beurt = 2;
+                                    label1.Text = "blauw is aan zet";
+                                }
+
+                                else
+                                {
+                                    beurt = 1;
+                                    label1.Text = "rood is aan zet";
+                                }
+                            }
+                            
+                    }
+                }    
+            }
+
+                
+
+
+
+            Panel.Invalidate();
+        }
+
+        bool mogelijk(int x, int y)
+        {
+            if (vak[x, y] == 0)
+            {
+
+                //het is mogelijk als aangrenzend vakje van [x,y] != beur
+                if (vak[x + 1, y] != beurt && vak[x + 1, y] != 0)
+                {
+                    for (int a = x + 1; a < hor; a++)
+                    {
+                        if (vak[a, y] == beurt)
+                            return true;
+                        
+                    }
+                    
+                }
+
+                else if (vak[x - 1, y] != beurt && vak[x - 1, y] != 0)
+                {
+                    for (int a = x - 1; a < hor; a++)
+                    {
+                        if (vak[a, y] == beurt)
+                            return true;
+                    }
+                }
+
+                else if (vak[x, y + 1] != beurt && vak[x, y + 1] != 0)
+                {
+                    for (int a = y + 1; a < hor; a++)
+                    {
+                        if (vak[x, a] == beurt)
+                            return true;
+                    }
+                }
+                else if (vak[x, y - 1] != beurt && vak[x, y - 1] != 0)
+                {
+                    for (int a = y - 1; a < hor; a++)
+                    {
+                        if (vak[x, a] == beurt)
+                            return true;
+                    }
+                }
+                else return false;
+            }
+            return false;
+
+
+        }
+        bool inrijx(int x, int y)
+        {
+
+            return true;
+        }
+        bool inrijy(int x, int y)
+        {
+            return true;
+        }
+        private void tekenveld(object sender, PaintEventArgs pea)
+            
+        {
+            for (int x = 0; x < hor; x++)
+            {
+                for (int y = 0; y < vert; y++)
+                {
+                    pea.Graphics.DrawRectangle(Pens.Black, 0 + x * (Panel.Size.Width / hor), 0 + y * (Panel.Size.Height / vert), Panel.Size.Width / hor, Panel.Size.Height / vert);
+                }
+            }
+        }
+
+        private void tekencirkel (object sender, PaintEventArgs pea)
+        {
+            for (int x = 0; x < hor; x++)
+            {
+                for (int y = 0; y < vert; y++)
+                {
+                    if (this.vak[x, y] == 1) 
+                        pea.Graphics.FillEllipse(Brushes.Red, 0 + x * (Panel.Size.Width / hor), 0 + y * (Panel.Size.Height / vert), Panel.Size.Width / hor, Panel.Size.Height / vert);
+                    if (this.vak[x, y] == 2)
+                        pea.Graphics.FillEllipse(Brushes.Blue, 0 + x * (Panel.Size.Width / hor), 0 + y * (Panel.Size.Height / vert), Panel.Size.Width / hor, Panel.Size.Height / vert);
+                        
+                }
+            } 
+        }
+
+    }
+}
